71fe215aa861164953105e6222ab34b053e3ea75
MIN0_KABE <HGA00562@nifty.com>
2025-04-30 00:26:22 +0000

    chenge

--- FIGtoRUBY_01_a.rb
+++ FIGtoRUBY_01_a.rb
@@ -19,9 +19,9 @@ end
 
 
 text = "前略\nお元気ですか?\n私は元気です。\n中略\nお体にお気をつけて。\n後略。"
-#text.each_line(chomp: true) {|line|
-# p "#{line}: #{line.length}文字"
-#}
+text.each_line(chomp: true) {|line|
+ p "#{line}: #{line.length}文字"
+}
 
 
 
