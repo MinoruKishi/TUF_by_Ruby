97ce3671f716ee6319cee0e79cb5bb1516987ee2
MIN0_KABE <HGA00562@nifty.com>
2025-05-07 10:55:07 +0900

    Update FIGtoRUBY_01_b.rb

--- FIGtoRUBY_01_b.rb
+++ FIGtoRUBY_01_b.rb
@@ -9,7 +9,16 @@ flg_sw = false
 input_file = File.open('fig.fth', 'r') do |f|
   f.each_line do |l|
 #    puts l
-   if t = l.match(/^SCR . (\d+)/)
-     if t[1]=="6"
+    if t = l.match(/^SCR . (\d+)/)
+      if t[1].to_i==6
+        flg_sw = true
+      end
+    elsif t = l.match(/^\s+(\d+)\s*(.*)$/)
+      if t[1].to_i>=6
+        puts t[1]+":" + t[2]
+      end
+    else
      
- 
+    end
+  end
+end
